75c3864b45f4504a64bc0203f258fd0ff0d72be5
LAU PUIKI <LAU PUIKI@192.168.0.4>
2022-12-03 12:52:55 -0500

    ImagePackage updated

---
+++ Evans_FinalProject/ImagePackage/Function.py
@@ -0,0 +1,15 @@
+'''
+Name: Team Evans
+email: laupi@mail.uc.edu 
+Assignment: Final Project
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that I can use Eclipse to create a PyDev project
+
+'''
+from PIL import Image
+
+def load_image(filename):
+    myimage = Image.open(filename)
+    myimage.load()
+    return myimage
---
+++ Evans_FinalProject/ImagePackage/Main.py
@@ -0,0 +1,13 @@
+'''
+Name: Team Evans
+email: laupi@mail.uc.edu 
+Assignment: Final Project
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that I can use Eclipse to create a PyDev project
+
+'''
+from Function import *
+
+im = Image.open("Team Evans.jpg")
+im.show() 
